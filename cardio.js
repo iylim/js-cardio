675a2c68f0afce5640552af594683d6383feb164
Ivy <yunjilim@gmail.com>
2019-02-07 16:16:49 -0800

    first

--- cardio.js
+++ cardio.js
@@ -21,7 +21,7 @@ const inventors = [
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
 // 1. Filter the list of inventors for those who were born in the 1500's
-
+// const oldPeople = inventors.filter(function(inventor) => )
 // 2. Give us an array of the inventors' first and last names
 
 // 3. Sort the inventors by birthdate, oldest to youngest
@@ -35,7 +35,17 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 // 7. Sum up the instances of each of these vehicles
 // prettier-ignore
 const data = ['car','car','truck','truck','bike','walk','car','van','bike','walk','car','van','car','truck'];
-
+var obj = {}
+const carCount = data.forEach(function(item) {
+  if (obj.hasOwnProperty(item)) {
+    obj[item] += 1;
+  } else obj[item] = 1;
+})
+console.log(obj)
+// const carCount = data.reduce(function(acc, item) {
+//   return (item === 'car' ? acc + 1 : acc) 
+// }, 0)
+console.log(carCount)
 // 8. is at least one person 19 or older?
 // 9. is everyone 19 or older?
 const team = [
